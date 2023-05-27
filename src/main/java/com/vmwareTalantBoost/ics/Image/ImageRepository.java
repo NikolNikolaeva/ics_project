c0492612febe413602d8e77384de19c0150ca0cf
Nikol <92273054+NikolNikolaeva@users.noreply.github.com>
2023-05-27 19:31:29 +0300

    Delete ImageRepository.java

--- src/main/java/com/vmwareTalantBoost/ics/Image/ImageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.vmwareTalantBoost.ics.Image;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ImageRepository extends JpaRepository<Image, Long> {
-}
