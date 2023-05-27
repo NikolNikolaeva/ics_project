9725ffe4716b2259761b5a92ceb66fa20dec97c4
Nikol <92273054+NikolNikolaeva@users.noreply.github.com>
2023-05-27 19:32:20 +0300

    Delete Image_tagRepository.java

--- src/main/java/com/vmwareTalantBoost/ics/Image/Image_tagRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vmwareTalantBoost.ics.Image;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repositorygit
-public interface Image_tagRepository extends JpaRepository<Image_tag,Long> {
-
-}
