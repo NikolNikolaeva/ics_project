c0494e10fc47451842403112e124e625421538f7
Vasilka Terzieva <vasilka.terzieva@paysafe.com>
2024-12-29 14:45:22 +0200

    add-rating-ui

--- Frontend/image/src/app/result/result.component.ts
+++ Frontend/image/src/app/result/result.component.ts
@@ -1,9 +1,14 @@
 import {Component, OnInit} from '@angular/core';
 import {ImageService} from "../services/image.service";
-import {Tag} from "../objects/tag";
 import {Image} from "../objects/image";
 import {ActivatedRoute, Router} from "@angular/router";
-import {HttpErrorResponse} from "@angular/common/http";
+import '@cds/core/icon/register.js';
+import { ClarityIcons, thumbsUpIcon, thumbsDownIcon, talkBubblesIcon, downloadIcon } from '@cds/core/icon';
+
+ClarityIcons.addIcons(thumbsUpIcon);
+ClarityIcons.addIcons(thumbsDownIcon);
+ClarityIcons.addIcons(talkBubblesIcon);
+ClarityIcons.addIcons(downloadIcon);
 
 @Component({
   selector: 'app-result',
@@ -16,7 +21,6 @@ export class ResultComponent implements OnInit {
 
   constructor(private imageService: ImageService, private route: ActivatedRoute,private router: Router) {
   }
-
 
   navigateToResultView(tag: string) {
     this.router.navigateByUrl(`/images/${tag}`);
@@ -32,5 +36,51 @@ export class ResultComponent implements OnInit {
       (image) => {
         this.imageToAnalyse = image;
       });
+  }
+
+  likeImageWithId(id: number | undefined) {
+    if (id !== undefined) {
+      this.imageService.getImageById(id).subscribe(
+        (image) => {
+          image.likes += 1;
+          this.imageService.updateImage(image).subscribe((updatedImage: Image) => this.imageToAnalyse = updatedImage)
+        },
+        (error) => {
+          console.error('Error liking the image:', error);
+        }
+      );
+    } else {
+      console.error('Image ID is undefined');
+    }
+  }
+
+  dislikeImageWithId(id: number | undefined) {
+    if (id !== undefined) {
+      this.imageService.getImageById(id).subscribe(
+        (image) => {
+          image.dislikes += 1;
+          this.imageService.updateImage(image).subscribe((updatedImage: Image) => this.imageToAnalyse = updatedImage)
+        },
+        (error) => {
+          console.error('Error unliking the image:', error);
+        }
+      );
+    } else {
+      console.error('Image ID is undefined');
+    }
+  }
+
+  //Not working as expected at the moment
+  downloadImage(imageUrl: string | undefined) {
+    if (imageUrl) {
+      const link = document.createElement('a');
+      link.href = imageUrl;
+      link.download = imageUrl.split('/').pop() ?? 'downloaded_image';
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    } else {
+      console.error('Image URL is not available');
+    }
   }
 }
